7043d1430b909d2d288311147387cae9262f4629
Peter Kong <bansuki@gmail.com>
2017-10-26 19:39:09 -0500

    finished reqs

--- src/Account.js
+++ src/Account.js
@@ -7,17 +7,47 @@ export default class Account extends Component {
     this.state = {
       balance: 0
     }
+
+    this.handleDeposit = this.handleDeposit.bind(this);
+    this.handleWithdraw = this.handleWithdraw.bind(this);
+    this.styleBalanceZero = this.styleBalanceZero.bind(this);
   }
+
+  handleDeposit(e) {
+    if(this.amount.value) {
+      let newBalance = parseFloat(this.state.balance) + parseFloat(this.amount.value);
+      this.setState({
+        balance: newBalance
+      });
+    }
+  }
+
+  handleWithdraw(e) {
+    if(this.amount.value) {
+      let newBalance = parseFloat(this.state.balance) - parseFloat(this.amount.value);
+      if(newBalance >= 0) {
+        this.setState({
+          balance: newBalance
+        });
+      }
+    }
+  }
+
+  styleBalanceZero() {
+    return `balance${this.state.balance === 0 ? ' zero' : ''}`
+  }
+
   render() {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">${this.state.balance}</div>
-        <input type="text" ref={amount => this.amount = amount} placeholder="enter an amount" />
+        <div className={this.styleBalanceZero()}>${this.state.balance}</div>
+        <input type="text" ref={amount => this.amount = amount}
+          placeholder="enter an amount" />
         <input type="button" value="Deposit"
-          onClick={e => this.setState({ balance: this.state.balance + 1 })} />
+          onClick={this.handleDeposit} />
         <input type="button" value="Withdraw"
-          onClick={e => this.setState({ balance: this.state.balance - 1 })} />
+          onClick={this.handleWithdraw} />
       </div>
     )
   }
