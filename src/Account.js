63588504659f74de4af614bf0f2a18f51ba812ac
Peter Kong <bansuki@gmail.com>
2017-10-26 19:15:58 -0500

    up and running and buttons work

--- src/Account.js
+++ src/Account.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      balance: 0
+    }
+  }
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balance">${this.state.balance}</div>
+        <input type="text" ref={amount => this.amount = amount} placeholder="enter an amount" />
+        <input type="button" value="Deposit"
+          onClick={e => this.setState({ balance: this.state.balance + 1 })} />
+        <input type="button" value="Withdraw"
+          onClick={e => this.setState({ balance: this.state.balance - 1 })} />
       </div>
     )
   }
